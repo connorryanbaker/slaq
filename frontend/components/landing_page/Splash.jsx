56d1d3d49d62d01afbbb7474d87bd9141e365cc5
Connor Baker <connorbaker@gmail.com>
2019-03-26 16:48:43 -0400

    configure for heroku

--- frontend/components/landing_page/Splash.jsx
+++ frontend/components/landing_page/Splash.jsx
@@ -31,7 +31,7 @@ class Splash extends React.Component {
           <p className="splash-footnote">Already using slaq? <Link className="splash-signin-link" to='/login'>Sign In.</Link></p>
         </div>
         <div>
-          <img className="splash-img-left" src="assets/slaq_landing1-325b5daab2481e4b8aed6b3b676a482ae90cf7715fc65ca840b83faa520022cf.jpg
+          <img className="splash-img-left splash-img" src="assets/slaq_landing1-325b5daab2481e4b8aed6b3b676a482ae90cf7715fc65ca840b83faa520022cf.jpg
 "/>
           <img className="splash-img-center" src="assets/slaq_landing2-ad63d80c5a92c55049a7a39ba0a79d90937e06ebedd202e5ef2c20aef651e016.jpg"/>
           <img className="splash-img-right" src="assets/slaq_landing3-5e61062ed1fa1ee54ad6099bbcb90d6e86e8f9e43f85d6974adab9938f5a7cfb.jpg"/>
