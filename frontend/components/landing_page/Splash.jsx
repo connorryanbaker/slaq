98966f37454f8080bdd6fb2c5f18167d819dd4aa
Connor Baker <connorbaker@gmail.com>
2019-03-27 09:50:55 -0400

    implement css grid on splash

--- frontend/components/landing_page/Splash.jsx
+++ frontend/components/landing_page/Splash.jsx
@@ -37,21 +37,15 @@ class Splash extends React.Component {
     return (
       <div className="splash-container">
         <h1 className="splash-header">Imagine what you'll<br/> accomplish together</h1>
-        <div className="splash-form-container">
-          <p className="splash-paragraph">slaq is a collaboration hub for work, no matter what work you do. It’s a place where conversations happen, decisions are made, and information is always at your fingertips. With slaq, your team is better connected.</p>
-          <div className="splash-email-input-container">
-            {errors}
-            <input className="splash-input" type="text" placeholder="Email address" onChange={this.update("input")} />
-            <Link to='/signup'><button onClick={this.handleSubmit} className='btn-purple splash-btn'>GET STARTED</button></Link>
-          </div>
-          <p className="splash-footnote">Already using slaq? <Link className="splash-signin-link" to='/login'>Sign In.</Link></p>
-        </div>
-        <div>
-          <img className="splash-img-left splash-img" src="assets/slaq_landing1-325b5daab2481e4b8aed6b3b676a482ae90cf7715fc65ca840b83faa520022cf.jpg
+        <p className="splash-paragraph">slaq is a collaboration hub for work, no matter what work you do. It’s a place where conversations happen, decisions are made, and information is always at your fingertips. With slaq, your team is better connected.</p>
+        {errors}
+        <input className="splash-input" type="text" placeholder="Email address" onChange={this.update("input")} />
+        <Link to='/signup' className='splash-link'><button onClick={this.handleSubmit} className='btn-purple splash-btn'>GET STARTED</button></Link>
+        <p className="splash-footnote">Already using slaq? <Link className="splash-signin-link" to='/login'>Sign In.</Link></p>
+        <img className="splash-img-left splash-img" src="assets/slaq_landing1-325b5daab2481e4b8aed6b3b676a482ae90cf7715fc65ca840b83faa520022cf.jpg
 "/>
-          <img className="splash-img-center splash-img" src="assets/slaq_landing2-ad63d80c5a92c55049a7a39ba0a79d90937e06ebedd202e5ef2c20aef651e016.jpg"/>
-          <img className="splash-img-right splash-img" src="assets/slaq_landing3-5e61062ed1fa1ee54ad6099bbcb90d6e86e8f9e43f85d6974adab9938f5a7cfb.jpg"/>
-        </div>
+        <img className="splash-img-center splash-img" src="assets/slaq_landing2-ad63d80c5a92c55049a7a39ba0a79d90937e06ebedd202e5ef2c20aef651e016.jpg"/>
+        <img className="splash-img-right splash-img" src="assets/slaq_landing3-5e61062ed1fa1ee54ad6099bbcb90d6e86e8f9e43f85d6974adab9938f5a7cfb.jpg"/>
       </div>
     );
   }
