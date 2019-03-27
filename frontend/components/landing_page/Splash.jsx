53103a640e8c7838683d0bc0d84ee568397a07a6
Connor Baker <connorbaker@gmail.com>
2019-03-27 12:00:03 -0400

    add preliminary css transforms to splash page

--- frontend/components/landing_page/Splash.jsx
+++ frontend/components/landing_page/Splash.jsx
@@ -11,17 +11,26 @@ class Splash extends React.Component {
       input: '',
       imgs: ["assets/slaq_landing1-325b5daab2481e4b8aed6b3b676a482ae90cf7715fc65ca840b83faa520022cf.jpg",
         "assets/slaq_landing2-ad63d80c5a92c55049a7a39ba0a79d90937e06ebedd202e5ef2c20aef651e016.jpg",
-        "assets/slaq_landing3-5e61062ed1fa1ee54ad6099bbcb90d6e86e8f9e43f85d6974adab9938f5a7cfb.jpg"]
+        "assets/slaq_landing3-5e61062ed1fa1ee54ad6099bbcb90d6e86e8f9e43f85d6974adab9938f5a7cfb.jpg"],
+      imgCaptions: ["Away transforms creativity and customer support with slaq", "Molly Moon creates a recipe for success with slaq", "Auto Desk fosters open communication with slaq"]
     }
     this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     setInterval(() => {
+      const imgs = Array.from(document.getElementsByClassName('splash-img'));
+      imgs.forEach((e) => {
+        e.classList.add('fade');
+        e.classList.remove('appear');
+      });
       let newImgs = this.state.imgs;
       newImgs.push(newImgs.shift());
+      let newCaptions = this.state.imgCaptions;
+      newCaptions.push(newCaptions.shift());
       this.setState({
-        imgs: newImgs
+        imgs: newImgs,
+        imgCaptions: newCaptions
       });
-    }, 3000);
+    }, 4000);
   }
 
   update(field) {
@@ -42,6 +51,23 @@ class Splash extends React.Component {
     }
   }
 
+  componentDidMount() {
+    const imgs = Array.from(document.getElementsByClassName('splash-img'));
+    imgs.forEach((e) => { 
+      e.classList.remove('fade'); 
+    });
+  }
+
+  componentDidUpdate() {
+    const imgs = Array.from(document.getElementsByClassName('splash-img'));
+    imgs.forEach(e => { 
+      if (Array.from(e.classList).includes('fade')) e.classList.add('fade');
+    });
+    setTimeout(() => {
+      imgs.forEach(e => e.classList.remove('fade'));
+    }, 500);
+  }
+
   render() {
     const errors = this.props.errors.length > 0 ? <div class="splash-errors">{this.props.errors[0]}</div> : ""
     return (
@@ -54,6 +80,7 @@ class Splash extends React.Component {
         <p className="splash-footnote">Already using slaq? <Link className="splash-signin-link" to='/login'>Sign In.</Link></p>
         <img className="splash-img-left splash-img" src={this.state.imgs[0]}/>
         <img className="splash-img-center splash-img" src={this.state.imgs[1]}/>
+        <figcaption className="splash-caption splash-img">{this.state.imgCaptions[0]}</figcaption>
         <img className="splash-img-right splash-img" src={this.state.imgs[2]}/>
       </div>
     );
