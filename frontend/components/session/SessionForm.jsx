4475d08abbb11ee81dead6f6356353ee4242fd3d
Connor Baker <connorbaker@gmail.com>
2019-03-27 16:40:50 -0400

    style session-errors list

--- frontend/components/session/SessionForm.jsx
+++ frontend/components/session/SessionForm.jsx
@@ -59,19 +59,20 @@ class SessionForm extends React.Component {
     const emailErrors = errClasses.includes("Email") ? "session-error" : ""
     const passwordErrors = errClasses.includes("Password") ? "session-error" : ""
     const signInErr = errClasses.includes("Invalid") ? "session-error" : ""
+
     const errors = this.props.errors.map((e) => {
       return <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" className="warning"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z" /></svg>
               {e}</li>
     });
     const heading = (<p>Enter your {name}<b>email</b> and <b>password</b>.</p>)
-    const errClassname = this.props.errors.length > 0 ? "session-form-container" : ""
+    const errClassname = this.props.errors.length > 0 ? "session-form-container red-left" : ""
 
 
     return (
       <div>
         <div>
           <div className={errClassname}>
-            {this.props.errors.length > 0 ? <ul>{errors}</ul> : ""}
+            {this.props.errors.length > 0 ? <ul className="session-error-list">{errors}</ul> : ""}
           </div>
         </div>
         <div className="session-form-container">
