c171ee2f4dbb5916a08be320c59bf67fdbac934f
Connor Baker <connorbaker@gmail.com>
2019-04-01 10:11:05 -0400

    prevent dead img_urls from displaying

--- frontend/components/messages/MessageHeader.jsx
+++ frontend/components/messages/MessageHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MessageHeader = (props) => {
   return (
     <div className="message-user-img-row">
-      <img className="avatar-img" src={props.img_url} />
+      {props.img_url.length > 0 ? <img className="avatar-img" src={props.img_url} /> : ""}
       <div className="msg-user-container">
         <div className="name-time"><b className="msg-username">{props.username}</b>
           <span className="msg-user-time">{props.message.time}</span>
