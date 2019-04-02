dd546747c3ecba140f955d03efaf55dbbc05b771
Connor Baker <connorbaker@gmail.com>
2019-04-02 16:26:12 -0400

    add validations for length on channel name and message content

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,6 +2,6 @@ class Channel < ApplicationRecord
   has_many :messages, as: :messageable, dependent: :destroy
   has_many :channel_memberships, dependent: :destroy
   has_many :users, through: :channel_memberships
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
   belongs_to :creator, foreign_key: :creator_id, class_name: :User
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   validates :content, :user_id, presence: true 
+  validates :content, length: { maximum: 100 }
   belongs_to :messageable, polymorphic: true
   belongs_to :user
 end
