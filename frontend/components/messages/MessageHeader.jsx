a2290933d866f87a1fcb2d188fb0e488bd70adf1
Connor Baker <connorbaker@gmail.com>
2019-04-02 22:19:02 -0400

    fix time formatting

--- frontend/components/messages/MessageHeader.jsx
+++ frontend/components/messages/MessageHeader.jsx
@@ -4,26 +4,30 @@ class MessageHeader extends React.Component {
   constructor(props) {
     super(props)
     this.parseTime = this.parseTime.bind(this);
+    this.time = this.parseTime(this.props.message.created_at)
   }
 
   parseTime(time) {
-    const extractedTime = time.split("T")[1].slice(0, 5);
-    let formattedTime;
+    let extractedTime = time.split("T")[1].slice(0,5);
     if (extractedTime[0] == "0") {
-      formattedTime = extractedTime.slice(1).concat(" AM");
-    } else if (extractedTime[0] == "1") {
-      formattedTime = parseInt(extractedTime.slice(0, 2)) > 12 ? (parseInt(extractedTime.slice(0, 2) - 12).toString().concat(extractedTime.slice(2))).concat(" PM") : extractedTime.concat(" PM");
+      return extractedTime.slice(1).concat(" AM");
+    } else {
+      if (parseInt(extractedTime.slice(0, 2)) > 12) {
+        return parseInt(extractedTime.slice(0, 2) - 12).toString().concat(extractedTime.slice(2)).concat(" PM")
+      } else {
+        return extractedTime.concat(" PM");
+      }
     }
-    return formattedTime;
   }
 
   render() {
+    console.log(this.time);
     return (
       <div className="message-user-img-row">
         {this.props.img_url.length > 0 ? <img className="avatar-img" src={this.props.img_url} /> : ""}
         <div className="msg-user-container">
           <div className="name-time"><b className="msg-username">{this.props.username}</b>
-            <span className="msg-user-time">{this.parseTime(this.props.message.created_at)}</span>
+            <span className="msg-user-time">{this.time}</span>
           </div>
         </div>
       </div>
