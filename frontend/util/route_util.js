c7056ef0b538face3b364339759622247fe977b9
Connor Baker <connorbaker@gmail.com>
2019-04-03 08:40:34 -0400

    change redirect path for auth routes

--- frontend/util/route_util.js
+++ frontend/util/route_util.js
@@ -8,7 +8,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => {
       !loggedIn ? (
       <Component {...props} />
     ) : (
-      <Redirect to="/messages" />
+      <Redirect to="/messages/1" />
     )
     )}/>
   )
